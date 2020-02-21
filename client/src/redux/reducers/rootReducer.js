cd389a626304c8326bc724390b2f5021f5ace1aa
vimalvyshagam <vimalvyshagam@gmail.com>
2020-02-21 12:37:10 -0800

    delete all files

--- client/src/redux/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import confMesgReducer from "./confMesgReducer";
-
-const rootReducer = combineReducers({
-  messages: confMesgReducer
-});
-
-export default rootReducer;
--- config/key.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // mongoURI: "mongodb://127.0.0.1:27017/vvitConfessions"
-  mongoURI: "mongodb://vimal:confession1@ds147451.mlab.com:47451/confession"
-};
